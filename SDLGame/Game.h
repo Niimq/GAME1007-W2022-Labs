0fb9173e28a46f4118b7ba5342396749ceee390f
JossKM <31990727+JossKM@users.noreply.github.com>
2022-03-17 12:25:37 -0400

    Lab 10-2 starting. Updated comments

--- SDLGame/Game.h
+++ SDLGame/Game.h
@@ -1,3 +1,26 @@
+//////
+//
+// Welcome to the lab! Today we will add more sprites and shooting mechanics to the game
+// The lab begins at 12:10. Please be prepared with your projects
+// If you need it for reference, the code for SDL setup has been pushed to GitHub
+// 
+// Quiz 7 available
+// 
+// At the end of all the labs... we will do..
+// -Basic collision detection between sprites
+// -Hold space to shoot
+// -Spawn targets to shoot
+// -Detect when player touches a target
+// -Detect when player bullet touches a target
+// -Play music and sounds
+// 
+// Will NOT show you in the labs
+// -How to make enemies shoot back
+// -Infinitely scrolling background
+// -Sorting
+// -Screen boundaries
+//////
+
 #pragma once
 #include <SDL.h> 
 #include "Sprite.h"
